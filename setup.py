52e82de97a4aca467506e892cae190d4e3f15a23
Inti Ocean <me@inti.co>
2015-07-13 21:17:24 +0100

    minor changes to setup.cfg, correct email & url, typos and add some test requirements

--- setup.py
+++ setup.py
@@ -14,16 +14,20 @@ setup(name='pyinter',
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=['Development Status :: 3 - Alpha',
                    'License :: OSI Approved :: MIT License'],
-      keywords='interval range discontinous-range union intersection',
+      keywords='interval range discontinuous-range union intersection',
       author='Inti Ocean',
-      author_email='intiocean@gmail.com',
-      url='intiocean.com',
+      author_email='me@inti.co',
+      url='https://github.com/intiocean/pyinter',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=True,
       install_requires=[
           # -*- Extra requirements: -*-
+      ],
+      tests_require=[
+          'pytest',
+          'pytest-cov',
       ],
       entry_points="""
       # -*- Entry points: -*-
